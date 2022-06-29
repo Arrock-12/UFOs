1ea239b8332caea3e7fad5bb6d401bc9984d6995
Ashley R <101822948+Arrock-12@users.noreply.github.com>
2022-06-28 19:20:57 -0500

    added style elements

--- static/js/app.js
+++ static/js/app.js
@@ -40,7 +40,7 @@ function handleClick() {
 }
 
 // Attach an event to listen for the form button
-d3.selectAll("filter-btn").on("click", handleClick);
+d3.selectAll("#filter-btn").on("click", handleClick);
 
 // Buld the table when the page loads
 buildTable(tableData);
